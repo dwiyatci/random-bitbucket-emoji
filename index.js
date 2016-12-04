051655bae763aa30251b9a994865ce85a897b39c
Glenn Dwiyatcita <dwiyatci@mail.com>
2016-12-04 12:42:07 +0100

    fix(main): fix bbemojis.json path

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ module.exports = rbe;
 
 const _ = require('lodash');
 const { readFileSync } = require('fs');
-const emojis = JSON.parse(readFileSync('bbemojis.json', 'utf8'));
+const { resolve } = require('path');
+const emojis = JSON.parse(readFileSync(resolve(__dirname, 'bbemojis.json'), 'utf8'));
 
 function rbe() {
   return `:${emojis[_.round(_.random(0, emojis.length, true))]}:`;
